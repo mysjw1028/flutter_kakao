e725688339c01f2f84505e208eac60f8ed5fd044
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-11-11 11:55:00 +0900

    친구 목록보기

--- lib/screens/friend_screen.dart
+++ lib/screens/friend_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/profile_card.dart';
+import 'package:flutter_kakao/modles/user.dart';
 
 class FriendScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("FriendScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("친구"),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          ProfileCard(user: me),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              children: [
+                Text("친구"),
+                const SizedBox(width: 6),
+                Text("${friends.length}"),
+              ],
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: List.generate(
+              friends.length,
+              (index) => ProfileCard(user: friends[index]),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
